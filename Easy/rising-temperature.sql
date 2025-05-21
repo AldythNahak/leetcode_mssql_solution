d26396855cf3179f9fb86a15146b36e645f89eb8
Aldyth Nahak <49281877+AldythNahak@users.noreply.github.com>
2025-05-21 16:00:53 +0700

    add case

--- Easy/rising-temperature.sql
+++ Easy/rising-temperature.sql
@@ -9,3 +9,10 @@ insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25
 insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
 insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
 
+WITH collectTemp AS (
+	SELECT id, recordDate, temperature, LEAD(temperature) OVER(ORDER BY recordDate DESC) AS n_temp, 
+		LEAD(recordDate) OVER(ORDER BY recordDate DESC) AS n_record_date
+	FROM Weather
+) SELECT id FROM collectTemp
+WHERE temperature > n_temp AND DATEDIFF(DAY,n_record_date, recordDate) = 1
+ORDER BY id ASC
